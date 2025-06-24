1b9530afbb7a17b8a6cc07e18758d500ebe32d5c
varunkm11 <varunkumarsingh818@gmail.com>
2025-06-24 23:15:26 +0530

    Update forms.py

---
+++ forms.py
@@ -0,0 +1,3 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, FloatField, SelectField, SubmitField, DateField, TextAreaField
+from wtforms.validators import InputRequired, Email, Length, NumberRange, Optional
